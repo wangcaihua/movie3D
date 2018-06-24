260dcf1b9149568d538221d2caa0d9fce3ac5111
fitz <490419716@qq.com>
2018-06-24 15:54:00 +0800

    data structure design

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
-from os import environ
-
+import json
 from flask import Flask, url_for
+from flask import request, make_response, Response
 from flask import render_template
 
 app = Flask(__name__)
@@ -13,27 +13,67 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html',
-                           powered_by=environ.get('POWERED_BY', 'Deis'))
+    return render_template('index.html')
 
 '''
-2. path varables: /path/<converter:varname>
-    - converter: string, int, float, path, any, uuid
-    - varname: varable name
+{
+    "上证指数": [point, various, label],
+    "恒生指数": [point, various, label],
+    "道琼斯指数": [point, various, label],
+    ------
+    "保险": [point, various, label],
+    "半导体": [点数, 涨跌幅, 标记],
+    ......
+}
 '''
 
 
-@app.route('/<username>')
-def name(username):
-    return render_template('index.html',
-                           powered_by=username)
+@app.route('/get_index_and_block', methods=["GET"])
+def get_index_and_block():
+    pass
 
 
 '''
-3. HTTP method
+{
+    "code": [name, price, various],
+    "code": [name, price, various],
+    ......
+}
 '''
+
+
+@app.route('/get_stocks_in_block', methods=["GET"])
+def get_stocks_in_block():
+    pass
+
+'''
+{
+    kline: [
+                [time, open, high, close, low],
+                [time, open, high, close, low],
+                [time, open, high, close, low],
+            ],
+    mavg: [
+                [time, ma5, ma10, ma20, predict],
+                [time, ma5, ma10, ma20, predict],
+                [time, ma5, ma10, ma20, predict]
+            ],
+    volume: [
+                [time, volume],
+                [time, volume],
+                [time, volume]
+            ],
+    starttime: "2005-12-9",
+    stoptime: "2018-5-21",
+    length: "3908"
+}
+'''
+
+
+@app.route('/get_stock_data', methods=["GET"])
+def get_stock_data():
+    pass
+
 
 if __name__ == '__main__':
-    # Bind to PORT if defined, otherwise default to 5000.
-    port = int(environ.get('PORT', 5000))
-    app.run(host='localhost', port=port)
+    app.run(host='localhost', port=5000, debug=True)
